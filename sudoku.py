2b38562c9be4356f3a38af9264f79e2987ea8ddb
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2020-02-20 01:42:41 +0100

    Add sudoku solver

--- sudoku.py
+++ sudoku.py
@@ -17,3 +17,27 @@ def is_valid(num, pos, board):
         for j in range(3*dy, 3*dy + 3):
             if board[i][j] == num: return False
     return True
+
+def next_position(board, x, y):
+    return (x + 1, y) if x + 1 < 9 else (0, y + 1)
+
+def find_empty_cell(board, start_pos = (0, 0)):
+    x, y = start_pos
+    while x < 9 and y < 9 and board[x][y] != None:
+        x, y = next_position(board, x, y)
+    return x, y
+
+def solve_sudoku(board, pos = (0, 0)):
+    x, y = find_empty_cell(board, pos)
+    # If all gaps have been filled, return True
+    if x >= 9 or y >= 9: return True
+    # Find all valid values
+    values = [v for v in range(1, 10) if is_valid(v, (x, y), board)]
+    while len(values) > 0:
+        # Put them in the cell one by one
+        board[x][y] = values[0]
+        # Try to fill the other gaps
+        if solve_sudoku(board, find_empty_cell(board, (x, y))): return True
+        # Remove values which do not lead to a solution
+        values.pop(0)
+    return False
--- sudoku_test.py
+++ sudoku_test.py
@@ -1,5 +1,5 @@
-from sudoku import is_valid
-import unittest
+from sudoku import is_valid, next_position, find_empty_cell, solve_sudoku
+import unittest, copy
 
 class IsValidTest(unittest.TestCase):
 
@@ -24,3 +24,57 @@ class IsValidTest(unittest.TestCase):
     def test_num_in_another_box(self):
         self.board[0][0] = 1
         self.assertTrue(is_valid(num = 1, pos = (5, 6), board = self.board))
+
+class NextPositionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.board = [[None for i in range(9)] for j in range(9)]
+
+    def test_inner_pos(self):
+        self.assertEqual(next_position(self.board, 2, 2), (3, 2))
+
+    def test_outer_pos(self):
+        self.assertEqual(next_position(self.board, 8, 4), (0, 5))
+
+class FindEmptyCellTest(unittest.TestCase):
+
+    def test_empty_board(self):
+        board = [[None for i in range(9)] for j in range(9)]
+        self.assertEqual(find_empty_cell(board), (0, 0))
+
+    def test_nonempty_board(self):
+        board = [[2 for i in range(9)] for j in range(9)]
+        board[5][4] = None
+        self.assertEqual(find_empty_cell(board, start_pos = (1, 4)), (5, 4))
+
+    def test_board_without_gaps(self):
+        board = [[2 for i in range(9)] for j in range(9)]
+        self.assertEqual(find_empty_cell(board), (0, 9))
+
+class SolveSudokuTest(unittest.TestCase):
+
+    solution = [[8, 2, 7, 1, 5, 4, 3, 9, 6],
+                [9, 6, 5, 3, 2, 7, 1, 4, 8],
+                [3, 4, 1, 6, 8, 9, 7, 5, 2],
+                [5, 9, 3, 4, 6, 8, 2, 7, 1],
+                [4, 7, 2, 5, 1, 3, 6, 8, 9],
+                [6, 1, 8, 9, 7, 2, 4, 3, 5],
+                [7, 8, 6, 2, 3, 5, 9, 1, 4],
+                [1, 5, 4, 7, 9, 6, 8, 2, 3],
+                [2, 3, 9, 8, 4, 1, 5, 6, 7]]
+
+    def setUp(self):
+        self.board = copy.deepcopy(SolveSudokuTest.solution)
+
+    def test_single_gap(self):
+        self.board[4][7] = None
+        solve_sudoku(self.board)
+        self.assertListEqual(self.board, SolveSudokuTest.solution)
+
+    def test_multiple_gaps(self):
+        self.board[1][3] = None
+        self.board[5][2] = None
+        self.board[7][6] = None
+        self.board[4][8] = None
+        solve_sudoku(self.board)
+        self.assertListEqual(self.board, SolveSudokuTest.solution)
